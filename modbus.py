efee79bb4c27deb98debd88ef8e5471b0d07c2fa
Brandon Curtis <brandon.curtis@gmail.com>
2017-02-07 05:18:28 -0500

    working logger

--- modbus.py
+++ modbus.py
@@ -1,20 +1,53 @@
 #!/usr/bin/env python3
 
-import minimalmodbus as mm
+import minimalmodbus
+import serial
+import time
+import datetime
 
-for add in [1]:
-# for add in range(0,247):
-    for baud in [9600]:
-#    for baud in [300,1200,2400,4800,9600,14400,19200,28800,38400,57600,115200]:
-        instrument = mm.Instrument(port='/dev/ttyUSB0', slaveaddress=add)
-        instrument.serial.timeout = 1.00
-        instrument.serial.baudrate = baud
-        #print(instrument)
+mydelay = 1
+myunits = 'g'
 
-        for reg in range(0,5):
-            try:
-                print(instrument.read_register(registeraddress=reg, functioncode=4))
-                print(instrument)
-            except IOError:
-                #print("Failed to read from instrument at {}".format(instrument.address))
-                print('.', end='', flush=True)
+usbscale = serial.Serial('/dev/usbscale',
+                      baudrate=57600,
+                      bytesize=serial.EIGHTBITS,
+                      parity=serial.PARITY_NONE,
+                      stopbits=serial.STOPBITS_ONE,
+                      timeout=1,
+                      xonxoff=0,
+                      rtscts=0
+                      )
+
+probetrans = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+probetrans.serial.baudrate = 9600
+probetrans.serial.timeout = 1.0
+
+myfile = datetime.datetime.today().isoformat()[:16]
+
+while True:
+    usbscale.write("*P".encode('utf8'))
+    time.sleep(0.1)
+    u = usbscale.readline().decode('utf8').split()[-1]
+    if (u == myuints):
+        break
+    time.sleep(0.1)
+    usbscale.write("*C".encode('utf8'))
+
+
+with open(myfile,'w') as mylog:
+    while True:
+        ts = time.time()
+        usbscale.write("*P".encode('utf8'))
+        val_scale = ' '.join(usbscale.readline().decode('utf8').split())
+        val_temp = probetrans.read_register(registeraddress=1,
+			                    numberOfDecimals=1,
+                                            functioncode=4,
+                                            signed=True)
+        val_do = probetrans.read_register(registeraddress=2,
+                                          numberOfDecimals=3,
+                                          functioncode=4,
+                                          signed=True)
+        myline = "{:.2f}, {!s} \u00B0C, {:.2} ppm, {}\n".format(ts,val_temp,val_do,val_scale)
+        mylog.write(myline)
+        mylog.flush()
+        time.sleep(mydelay)
